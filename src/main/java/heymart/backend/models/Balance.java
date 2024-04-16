56dfb559fb20c0f5a1b7edd312cd637602afbb57
iqzaardiansyah <iqzaardiansyah@gmail.com>
2024-04-16 15:55:20 +0700

    [GREEN] All tests for Balance model is passed

--- src/main/java/heymart/backend/models/Balance.java
+++ src/main/java/heymart/backend/models/Balance.java
@@ -1,5 +1,30 @@
 package heymart.backend.models;
 
-public class Balance {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "balance",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "ownerId")
+        })
+public class Balance {
+    @Id
+    private Long ownerId;
+
+    @Column(name = "balance_amount", columnDefinition = "bigint default 0")
+    @NotNull
+    private Long balance;
+
+    public Balance() {
+    }
+
+    public Balance(Long ownerId, Long balance) {
+        this.ownerId = ownerId;
+        this.balance = balance;
+    }
 }
