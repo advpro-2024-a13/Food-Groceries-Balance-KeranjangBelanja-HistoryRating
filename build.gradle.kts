4cc5941c4dec2429dea8fa80254dd8f9108ca643
iqzaardiansyah <iqzaardiansyah@gmail.com>
2024-04-16 15:48:29 +0700

    [RED] Implement tests for Balance models

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    java
+    jacoco
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "heymart.backend"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.springframework.boot:spring-boot-starter-validation")
+    runtimeOnly("org.postgresql:postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.register<Test>("unitTest") {
+    description = "Runs unit tests."
+    group = "verification"
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+}
---
+++ src/main/java/heymart/backend/BalanceKeranjangBelanjaHistoryRatingApplication.java
@@ -0,0 +1,13 @@
+package heymart.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceKeranjangBelanjaHistoryRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalanceKeranjangBelanjaHistoryRatingApplication.class, args);
+    }
+
+}
