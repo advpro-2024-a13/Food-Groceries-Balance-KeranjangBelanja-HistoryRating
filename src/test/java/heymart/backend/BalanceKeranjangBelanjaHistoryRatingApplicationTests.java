09f00d6b3be0d9df6692406332b2ac477b02fd78
iqzaardiansyah <iqzaardiansyah@gmail.com>
2024-04-17 11:06:07 +0700

    [GREEN] Fix on BalanceKeranjangBelanjaHistoryRatingApplicationTests that makes test failed

--- src/test/java/heymart/backend/BalanceKeranjangBelanjaHistoryRatingApplicationTests.java
+++ src/test/java/heymart/backend/BalanceKeranjangBelanjaHistoryRatingApplicationTests.java
@@ -2,8 +2,10 @@ package heymart.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = BalanceKeranjangBelanjaHistoryRatingApplication.class)
 class BalanceKeranjangBelanjaHistoryRatingApplicationTests {
 
     @Test
