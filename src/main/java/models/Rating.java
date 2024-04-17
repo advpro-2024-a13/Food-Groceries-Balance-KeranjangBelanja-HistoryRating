b6b844fe705a23f1d826fc7616725d8cfb0b3b3c
arjunajaa <arju.naja@ui.ac.id>
2024-04-17 19:07:28 +0700

    [GREEN] All tests for History model and Rating model is passed

--- src/main/java/models/Rating.java
+++ src/main/java/models/Rating.java
@@ -1,9 +1,6 @@
 package models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
