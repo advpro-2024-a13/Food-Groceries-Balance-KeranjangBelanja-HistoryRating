fe7fe4c500bb9a13ed447c463d80f19629650792
Iqza Ardiansyah <124988403+iqzaardiansyah@users.noreply.github.com>
2024-05-06 19:53:01 +0700

    [CHORE] Attempting to fix GitHub Action github-ci.yml and issues with Sonarcloud analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-gcp-starter:1.1.1.RELEASE")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation(kotlin("script-runtime"))
 }
 
 tasks.register<Test>("unitTest") {
@@ -59,4 +60,9 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+
+    reports {
+        html.required = true
+        xml.required = true
+    }
 }
