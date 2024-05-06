745329bb4ce669d9e97282fd7abf2f8a39e56f6d
arjunajaa <arju.naja@ui.ac.id>
2024-05-07 01:49:26 +0700

    [REFACTOR] Fixed bug that existed due to merging

--- src/test/java/heymart/backend/repository/HistoryRepositoryTest.java
+++ src/test/java/heymart/backend/repository/HistoryRepositoryTest.java
@@ -4,16 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import heymart.backend.models.History;
-import heymart.backend.service.HistoryServiceImpl;
+import heymart.backend.models.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +21,8 @@ public class HistoryRepositoryTest {
 
     @Mock
     private HistRepository histRepository;
+
+
     private List<History> histories;
 
     @BeforeEach
@@ -58,34 +59,5 @@ public class HistoryRepositoryTest {
         Optional<List<History>> foundHistories = histRepository.findByMarketId(marketId);
 
         assertEquals(expectedHistories, foundHistories.get());
-    
-    @Test
-    public void testFindByOwnerId() {
-        History history = new History();
-        history.setOwnerId(123L);
-        history.setMarketId(456L);
-
-        when(histRepository.findByOwnerId(123L)).thenReturn(Optional.of(List.of(history)));
-
-        Optional<List<History>> foundHistory = histRepository.findByOwnerId(123L);
-
-        assertTrue(foundHistory.isPresent());
-        assertEquals(123L, foundHistory.get().getFirst().getOwnerId());
-        assertEquals(456L, foundHistory.get().getFirst().getMarketId());
-    }
-
-    @Test
-    public void testFindByMarketId() {
-        History history = new History();
-        history.setOwnerId(123L);
-        history.setMarketId(456L);
-
-        when(histRepository.findByMarketId(456L)).thenReturn(Optional.of(List.of(history)));
-
-        Optional<List<History>> foundHistory = histRepository.findByMarketId(456L);
-
-        assertTrue(foundHistory.isPresent());
-        assertEquals(123L, foundHistory.get().getFirst().getOwnerId());
-        assertEquals(456L, foundHistory.get().getFirst().getMarketId());
     }
 }
