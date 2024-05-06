1ef3b0eaec31aa25fe14f4d6bc43a9531f8cbc3a
Iqza Ardiansyah <124988403+iqzaardiansyah@users.noreply.github.com>
2024-05-07 01:24:57 +0700

    Revert "merge history rating"

--- src/main/java/heymart/backend/service/KeranjangBelanjaService.java
+++
@@ -1,7 +0,0 @@
-package heymart.backend.service;
-
-import heymart.backend.models.KeranjangBelanja;
-
-public interface KeranjangBelanjaService {
-    KeranjangBelanja createKeranjangBelanja(KeranjangBelanja keranjangBelanja);
-}
