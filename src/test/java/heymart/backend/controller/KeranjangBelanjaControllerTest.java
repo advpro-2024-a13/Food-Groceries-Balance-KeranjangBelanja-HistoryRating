be7db17d1ab40861d4f2ef802aaf5b66fc4908f6
Iqza Ardiansyah <124988403+iqzaardiansyah@users.noreply.github.com>
2024-05-24 19:45:15 +0700

    [CHORE] Fix failed test file

--- src/test/java/heymart/backend/controller/KeranjangBelanjaControllerTest.java
+++ src/test/java/heymart/backend/controller/KeranjangBelanjaControllerTest.java
@@ -1,42 +1,45 @@
 package heymart.backend.controller;
 
 import heymart.backend.models.KeranjangBelanja;
-import heymart.backend.service.KeranjangBelanjaServiceImpl;
+import heymart.backend.service.KeranjangBelanjaService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class KeranjangBelanjaControllerTest {
+
+    @Mock
+    private KeranjangBelanjaService keranjangBelanjaService;
 
     @InjectMocks
     private KeranjangBelanjaController keranjangBelanjaController;
 
     @BeforeEach
-    void setKeranjangBelanja(){
-        MockitoAnnotations.openMocks(this);
+    void setUp() {
+        keranjangBelanjaController = new KeranjangBelanjaController(keranjangBelanjaService);
     }
 
     @Test
-    void testCreateKeranjangBelanjaPage(){
+    void testCreateKeranjangBelanjaPage() {
         Model model = mock(Model.class);
         String result = keranjangBelanjaController.createKeranjangBelanjaPage(model);
         assertEquals("CreateKeranjangBelanja", result);
     }
 
     @Test
-    void testCreateKeranjangBelanjaPost(){
-        Model model = mock(Model.class);
+    void testCreateKeranjangBelanjaPost() {
         KeranjangBelanja keranjangBelanja = new KeranjangBelanja();
+        when(keranjangBelanjaService.createKeranjangBelanja(keranjangBelanja)).thenReturn(keranjangBelanja);
         String result = keranjangBelanjaController.createKeranjangBelanjaPost(keranjangBelanja);
         assertEquals("redirect::list", result);
+        verify(keranjangBelanjaService, times(1)).createKeranjangBelanja(keranjangBelanja);
     }
 }
